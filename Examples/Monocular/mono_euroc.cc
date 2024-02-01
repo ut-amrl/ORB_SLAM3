e3d43cb400af17b88951f47f8f90775535ee9021
Dongmyeong Lee <domlee@utexas.edu>
2024-02-01 17:56:08 -0600

    move configuration files

--- Examples/Monocular/mono_euroc.cc
+++ Examples/Monocular/mono_euroc.cc
@@ -86,7 +86,7 @@ int main(int argc, char **argv) {
   float dT = 1.f / fps;
   // Create SLAM system. It initializes all system threads and gets ready to process
   // frames.
-  ORB_SLAM3::System SLAM(argv[1], argv[2], ORB_SLAM3::System::MONOCULAR, false);
+  ORB_SLAM3::System SLAM(argv[1], argv[2], ORB_SLAM3::System::MONOCULAR, true);
   float imageScale = SLAM.GetImageScale();
 
   double t_resize = 0.f;
